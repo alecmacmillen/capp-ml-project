6362fbce9178bb28060dcddf4c77d4296b7222b7
Alec MacMillen <amacmillen@cs.uchicago.edu>
2019-06-08 15:37:12 -0500

    Removed all visualization functions from main pipeline module

--- Scripts/visualizations.py
+++ Scripts/visualizations.py
@@ -118,3 +118,93 @@ def plot_scatter(dataframe, x, y, outliers=True):
     title = "Plot of " + x + " against " + y
     plt.title(title)
     plt.show()
+
+
+
+###################
+# EVALUATION PLOTS
+###################
+def plot_confusion_matrix(
+    y_true, y_pred, classes, normalize=False, title=None, cmap=plt.cm.Greens):
+    '''
+    Plot confusion matrix with true/false positives and true/false negatives
+    to visualize prediction mistakes. Code adapted from: https://scikit-learn.org/
+    stable/auto_examples/model_selection/plot_confusion_matrix.html.
+
+    Inputs:
+      y_true (list): list of *true* class labels (target variable test data)
+      y_pred (list): list of *predicted* class labels (result of predict_proba
+        from a trained model)
+      normalize (Bool): if True, present confusion matrix figures as proportions.
+        If False, present as integers (raw count).
+      cmap (plt color): set the matplotlib color
+
+    Returns:
+      Plotted confusion matrix
+    '''
+    if not title:
+        if normalize:
+            title = "Normalized confusion matrix"
+        else:
+            title = "Confusion matrix, without normalization"
+
+    cm = metrics.confusion_matrix(y_true, y_pred)
+    #classes = classes[utils.multiclass.unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print("Confusion matrix, without normalization")
+
+    print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation="nearest", cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           xticklabels=classes, yticklabels=classes, title=title,
+           ylabel="True label", xlabel="Predicted label")
+
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right", 
+             rotation_mode="anchor")
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 1.5
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    #return ax
+
+
+def plot_roc_curve(fpr, tpr, roc_auc, color, title):
+    '''
+    Plot ROC curve on a matplotlib plot
+
+    Inputs:
+      fpr (array): false positives (x-points of ROC)
+      tpr (array): true positives (y-points of ROC)
+      roc_auc (float): area under the curve 
+      color (str): color for ROC curve line
+      title (str): plot title
+
+    Returns:
+      None, displays plot in-place
+    '''
+    plt.figure()
+    plt.plot(fpr, tpr, color=color, marker='.',
+        lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(title)
+    plt.legend(loc='lower right')
+    plt.show()
+
+
+
